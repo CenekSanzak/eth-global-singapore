23f6fa62fac7e0e074c70f6978191be8ce76ec00
dogukan <dogukanakarc@gmail.com>
2024-09-22 06:43:51 +0800

    add bash scripts for call

--- phala-backend/add_tool_call.sh
+++ phala-backend/add_tool_call.sh
@@ -1,4 +1,4 @@
 near call zimbirti.testnet \
 add_tool \
-'{"name": "zim", "deposit": "1000000000000000000000000"}' \
+'{"tool":{"query_url":"https://wapo-testnet.phala.network/ipfs/QmPYNmVuJqsX8QywmEH4Frquq4YXAAH29BKFyzaCUAuJnq?contractName=zimbirti.testnet&methodName=get_greeting&args=", "json_schema": "{\"type\":\"function\",\"function\":{\"name\":\"call_near_contract\",\"description\":\"Call a NEAR contract\",\"parameters\":{\"type\":\"object\",\"properties\":{\"contractName\":{\"type\":\"string\",\"description\":\"Name of the NEAR contract to call\"},\"methodName\":{\"type\":\"string\",\"description\":\"Name of the method to call on the contract\"},\"args\":{\"type\":\"string\",\"description\":\"Arguments to pass to the method\"}}}}}"}}' \
 --accountId zimbirti.testnet 
---
+++ phala-backend/upvote_tool_call.sh
@@ -0,0 +1,4 @@
+near call zimbirti.testnet \
+upvote_tool \
+'{"id": 0}' \
+--accountId zimbirti.testnet 
