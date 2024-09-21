45b53f1848d39458fe0beba7f80a4e4569c5cab2
ramazanoacar <ramazan.acar@boun.edu.tr>
2024-09-21 13:09:17 +0300

    change to next.js and worldId init

--- demo-app/src/components/ui/button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({ children, ...props }) => {
-  return (
-    <button {...props} className="rounded-md focus:outline-none">
-      {children}
-    </button>
-  );
-};
-
-export { Button };
